661f5db6f7562ef0244e84ce2ff88a4d9f7e5ec8
divya <divyakolluris@gmail.com>
2020-04-27 09:16:51 -0500

    Appicons changed for both IOS,Android and done with settings configuration for both sheltermain.dart & restaurantmain.dart

--- lib/restaurantDrawer/restaurantSettings.dart
+++ lib/restaurantDrawer/restaurantSettings.dart
@@ -35,13 +35,9 @@ class  Settings extends StatelessWidget {
                     alignment: Alignment.topLeft,
                     padding: EdgeInsets.only(top: 3.0),
                     child: Text(
-                      "Change your name,description and profile photo",style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold,
-                    ),
-                    ),
-                  )
-                ],
-              ),
-            ),
+                      "Change your name,description and profile photo.",style: TextStyle(fontSize: 10.0,fontWeight: FontWeight.bold,
+                    ),),)
+                ],),),
             Divider(
               height: 20.0,
               thickness: 0.5,
@@ -49,10 +45,33 @@ class  Settings extends StatelessWidget {
               indent: 0.0,
               endIndent: 0.0,
             ),
+            Column(
+              children: <Widget>[
+                Container(margin: EdgeInsets.only(top: 15.0, left: 15.0, right: 15.0),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      alignment: Alignment.topLeft,
+                      child: Text("Account Settings",style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold,
+                      ),),
+                    )
+                  ],),),
+                Container(
+                  alignment:Alignment.topLeft ,
+                  padding: EdgeInsets.only(top: 3.0),
+                  child: Text("Change your email or delete your account.",style: TextStyle(fontSize: 10.0,fontWeight:FontWeight.bold,
+                  ),
+                ),
+                )
+
+              ],
+
+            ),
           ],
         ),
 
       ),
+
 
     );
   }
