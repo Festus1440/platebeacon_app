b11a632750d01af0f56b43688b4430b877ad7826
anushree <agupta2@neiu.edu>
2020-04-29 18:44:37 -0500

    Help moved above logout in home menu

--- lib/ShelterMain.dart
+++ lib/ShelterMain.dart
@@ -244,6 +244,32 @@ class _MaterialHomeState extends State<Home> {
               title: Text("Settings"),
             ),
             ListTile(
+              onTap: () {    showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  // return object of type Dialog
+                  return AlertDialog(
+                    shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(15)),
+                    backgroundColor: Colors.white,
+                    title: new Text("Help coming soon!"),
+                    content: new Text("Yes, you might have questions and we'll have a help section shortly."
+                        " For now directly ask the team!"),
+                    actions: <Widget>[
+                      new FlatButton(
+                        child: new Text("Sounds good!"),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );},
+              leading: Container(
+                  margin: EdgeInsets.only(left: 10.0), child: Icon(Icons.help)),
+              title: Text("Help"),
+            ),
+            ListTile(
               onTap: () {
                 Navigator.pop(context);
                 FirebaseAuth.instance.signOut().then((value) {
